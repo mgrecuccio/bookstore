ccd7fd5c32b067b84178a69889f8bb0b7e08289f
mgrecuccio <marco.grecuccio@mgrtech.eu>
2025-03-12 22:43:16 +0100

    Adding pages app

---
+++ pages/tests.py
@@ -0,0 +1,37 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+from pages.views import HomePageView
+
+
+# Create your tests here.
+class HomePageTest(SimpleTestCase):
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+
+    def test_url_exists_at_correct_location(self):
+        self.assertEqual(self.response.status_code, 200)
+
+
+    def test_homepage_url_name(self):
+        self.assertEqual(self.response.status_code, 200)
+
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, 'home.html')
+
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, 'This is our homepage')
+
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'This should not be present')
+
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve("/")
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+
