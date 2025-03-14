02b8b9a49a0930a987f132f8a5a18b528c4dfd4b
Marco Grecuccio <159437931+mgrecuccio@users.noreply.github.com>
2025-03-14 06:29:52 +0100

    Adding bootstrap and registration pages

--- django_project/settings.py
+++ django_project/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third-party
+    "crispy_forms",
+    "crispy_bootstrap5",
     # Local
     "accounts.apps.AccountsConfig",
     "pages.apps.PagesConfig",
@@ -122,7 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -131,5 +137,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
-LOGIN_REDIRECT_URL = "home"
-LOGOUT_REDIRECT_URL = "home"
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
+#crispy_forms
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+CRISPY_ALLOWED_TEMPLATE_PACK = 'bootstrap5'
--- pages/tests.py
+++ pages/tests.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
-from pages.views import HomePageView
+from pages.views import HomePageView, AboutPageView
 
 
 # Create your tests here.
@@ -31,7 +31,33 @@ class HomePageTest(SimpleTestCase):
         self.assertNotContains(self.response, 'This should not be present')
 
 
-    def test_homepage_url_resolves_homepageview(self):
+    def test_homepage_url_resolves_homepage_view(self):
         view = resolve("/")
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
 
+
+class AboutPageTest(SimpleTestCase):
+    def setUp(self):
+        url = reverse('about')
+        self.response = self.client.get(url)
+
+
+    def test_about_page_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+
+    def test_about_page_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
+
+
+    def test_about_page_contains_correct_html(self):
+        self.assertContains(self.response, "About Page")
+
+
+    def test_about_page_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, "This content should not be there")
+
+
+    def test_about_page_url_resolves_about_page_view(self):
+        view = resolve("/about/")
+        self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
